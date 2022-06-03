5f7718c1b58529147bfac148e05b94188f05f38a
Andrew McCoy <agmccoy@dmacc.edu>
2022-06-03 02:18:42 -0500

    Add project files.

---
+++ FirstResponsiveWebAppMcCoy/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FirstResponsiveWebAppMcCoy.Models;
+
+namespace FirstResponsiveWebAppMcCoy.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.Name = "";
+            ViewBag.CurrentAge = 0;
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(AgeModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.CurrentAge = model.AgeThisYear();
+                ViewBag.Name = model.FullName;
+            }
+            else
+            {
+                ViewBag.CurrentAge = 0;
+            }
+            return View(model);
+        }
+    }
+}
---
+++ FirstResponsiveWebAppMcCoy/Models/AgeModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace FirstResponsiveWebAppMcCoy.Models
+{
+    public class AgeModel    
+    {
+        public DateTime date = DateTime.Now;
+
+        [Required(ErrorMessage = "Please enter a name.")]       
+        public string FullName { get; set; }
+
+        [Required(ErrorMessage ="Please enter the year you were born.")]
+        [Range(1,2022, ErrorMessage ="please enter a valid year.")]
+        public int? BirthYear { get; set; }
+
+        
+
+        //this method does the math to determine how old the individual will be on December 31 of this year
+        public int AgeThisYear()
+        {            
+            int currentYear = date.Year;
+            int yearsOld =  currentYear - BirthYear.Value;
+            return yearsOld;
+        }
+    }
+}
