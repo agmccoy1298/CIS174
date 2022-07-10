a6482ebbb17597e5c039ceb0b9270fb484565f92
Andrew Grant McCoy <andrewsmccoy@outlook.com>
2022-07-09 22:58:12 -0500

    added some stuff

---
+++ TopicFiveWebAppMcCoy/TopicFiveWebAppMcCoy/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TopicFiveWebAppMcCoy.Models
+{
+    [Keyless]
+    public class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Grade { get; set; }
+
+    }
+}
